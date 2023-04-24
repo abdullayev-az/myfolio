b9ad37c4ca19d700cef739efa7f3207800dc0c4c
Abdullayev.net <96205201+abdullayev-az@users.noreply.github.com>
2023-04-24 17:22:28 +0400

    element
    initial commit

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ $('.counter').counterUp({
     var taleh = document.querySelector(".nav-bar");
     taleh.classList.toggle("sticky", window.scrollY > 0);
 })
+
+(function () {
+    var options = {
+        whatsapp: "994997200067",
+        call_to_action: "Salam nece komek ede bierem?",
+        position: "left",
+    };
+    var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+})();
