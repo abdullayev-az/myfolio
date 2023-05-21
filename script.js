f45db7ee8a73cc157b23c78995d8ad2e20a715b5
Abdullayev.net <96205201+abdullayev-az@users.noreply.github.com>
2023-05-22 03:06:26 +0400

    Update script.js

--- script.js
+++ script.js
@@ -16,14 +16,69 @@ $('.counter').counterUp({
     taleh.classList.toggle("sticky", window.scrollY > 0);
 })
 
+$('.chatbox').click(function(){
+    $(this).css('background-color','red');
+});
+
 (function () {
     var options = {
         whatsapp: "994997200067",
         call_to_action: "Salam nece komek ede bierem?",
-        position: "left",
+        position: "right",
     };
     var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
     var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
     s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
     var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
 })();
+
+$('.item1').click(function(){
+    $('.sidebar').fadeIn();
+    $('html').css({'overflow-y':'hidden'});
+    $('.bank').css({'display':'block'});
+    $('.course').css({'display':'none'});
+    $('.cargo').css({'display':'none'});
+    $('.mall').css({'display':'none'});
+    $('.shop').css({'display':'none'});
+})
+$('.item2').click(function(){
+    $('.sidebar').fadeIn();
+    $('html').css({'overflow-y':'hidden'});
+    $('.course').css({'display':'block'});
+    $('.bank').css({'display':'none'});
+    $('.cargo').css({'display':'none'});
+    $('.mall').css({'display':'none'});
+    $('.shop').css({'display':'none'});
+})
+$('.item3').click(function(){
+    $('.sidebar').fadeIn();
+    $('html').css({'overflow-y':'hidden'});
+    $('.cargo').css({'display':'block'});
+    $('.bank').css({'display':'none'});
+    $('.course').css({'display':'none'});
+    $('.mall').css({'display':'none'});
+    $('.shop').css({'display':'none'});
+})
+$('.item4').click(function(){
+    $('.sidebar').fadeIn();
+    $('html').css({'overflow-y':'hidden'});
+    $('.mall').css({'display':'block'});
+    $('.bank').css({'display':'none'});
+    $('.course').css({'display':'none'});
+    $('.cargo').css({'display':'none'});
+    $('.shop').css({'display':'none'});
+})
+$('.item5').click(function(){
+    $('.sidebar').fadeIn();
+    $('html').css({'overflow-y':'hidden'});
+    $('.shop').css({'display':'block'});
+    $('.bank').css({'display':'none'});
+    $('.course').css({'display':'none'});
+    $('.cargo').css({'display':'none'});
+    $('.mall').css({'display':'none'});
+})
+$('.barclose').click(function(){
+    $('.sidebar').fadeOut();
+    $('html').css({'overflow-y':'scroll'});
+})
+
