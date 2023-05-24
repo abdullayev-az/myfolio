b9656365467fa924d49a99b27b3a6a597411c262
Abdullayev.net <96205201+abdullayev-az@users.noreply.github.com>
2023-05-25 01:03:40 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+AOS.init();
 $('.hamburger').click(function(){
     $('.nav-offcanvas').css('transform','translateX(0)')
 });
@@ -15,23 +16,6 @@ $('.counter').counterUp({
     var taleh = document.querySelector(".nav-bar");
     taleh.classList.toggle("sticky", window.scrollY > 0);
 })
-
-$('.chatbox').click(function(){
-    $(this).css('background-color','red');
-});
-
-(function () {
-    var options = {
-        whatsapp: "994997200067",
-        call_to_action: "Salam nece komek ede bierem?",
-        position: "right",
-    };
-    var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
-    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
-    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
-})();
-
 $('.item1').click(function(){
     $('.sidebar').fadeIn();
     $('html').css({'overflow-y':'hidden'});
@@ -80,5 +64,95 @@ $('.item5').click(function(){
 $('.barclose').click(function(){
     $('.sidebar').fadeOut();
     $('html').css({'overflow-y':'scroll'});
+    TweenMax.to(".sidebar", 1.5, {
+        width: "0%",
+        ease: Expo.easeInOut
+     }, 0.2)
+     TweenMax.to(".projects-main", 1, {
+        width: "0%",
+        ease: Expo.easeOut
+     }, 0.2)
 })
+
+
+
+
+const mediaQuery = window.matchMedia('(max-width: 576px)')
+if (mediaQuery.matches) {
+    $('.projects-item').click(function(){
+        TweenMax.to(".sidebar", 1.5, {
+            width: "100%",
+            ease: Expo.easeOutIn
+         }, 0.2)
+         TweenMax.to(".projects-main", 1, {
+            width: "80%",
+            delay: 1,
+            ease: Expo.easeIn
+         }, 0.2)
+         
+    });
+  }
+   const mediaQuerys = window.matchMedia('(min-width: 576px)')
+if (mediaQuerys.matches) {
+    $('.projects-item').click(function(){
+        TweenMax.to(".sidebar", 1.5, {
+            width: "100%",
+            ease: Expo.easeOutIn
+         }, 0.2)
+         TweenMax.to(".projects-main", 1, {
+            width: "60%",
+            delay: 1,
+            ease: Expo.easeIn
+         }, 0.2)
+         
+    });
+  }
+
+  const mediaQueryss = window.matchMedia('(min-width: 991px)')
+  if (mediaQueryss.matches) {
+      $('.projects-item').click(function(){
+          TweenMax.to(".sidebar", 1.5, {
+              width: "100%",
+              ease: Expo.easeOutIn
+           }, 0.2)
+           TweenMax.to(".projects-main", 1, {
+              width: "40%",
+              delay: 1,
+              ease: Expo.easeIn
+           }, 0.2)
+           
+      });
+    }
+
+
+
+
+
+
+
+/* 
+    (function () {
+        var options = {
+            whatsapp: "994997200067",
+            call_to_action: "Salam nece komek ede bierem?",
+            position: "left",
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })(); */
+
+    $('.talk').click(function(){
+       $('.chatbox').toggle('');
+    });
+
+    function togglebutton() {
+        var x = document.getElementById("change");
+        if (x.innerHTML === "Start Talking") {
+          x.innerHTML = "Stop Talking";
+        } else {
+          x.innerHTML = "Start Talking";
+        }
+      }
 
